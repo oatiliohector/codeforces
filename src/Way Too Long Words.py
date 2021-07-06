60eb825e627f83e52c299ad9430b14bcd142336b
Atilio <atiliogoulart@gmail.com>
2021-07-06 11:17:03 -0300

    Done

--- src/Way Too Long Words.py
+++ src/Way Too Long Words.py
@@ -3,10 +3,10 @@ times = int(input())
 for i in range(times):
     word = str(input())
 
-    if len(word) < 10:
+    if len(word) < 11:
         print(word)
     else:
-        for k in range(times):
+        for k in range(1):
             first_letter = word[0]
             last_letter = word[-1]
 
