452f4f6e6f18e1e43deeff102b10aee38fb07c59
Atilio <atiliogoulart@gmail.com>
2021-07-11 14:12:02 -0300

    Sounds better

--- src/158A-Next Round.py
+++ src/158A-Next Round.py
@@ -6,8 +6,8 @@ temp = numbers_scores[b-1]
 
 count = 0
 
-for i in range(0,len(numbers_scores)):
-    if numbers_scores[i] >= temp and numbers_scores[i]>0:
+for i in range(0, len(numbers_scores)):
+    if numbers_scores[i] >= temp and numbers_scores[i] > 0:
         count += 1
 
-print(count)    
+print(count)
